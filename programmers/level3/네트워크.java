b94eeb000cb30c6aa4dba42cfda5aba700097604
yuzi0 <zag001@naver.com>
2020-03-02 00:46:03 +0900

    2020/02/29 스터디 문제 코드 수정

--- programmers/level3/네트워크.java
+++ programmers/level3/네트워크.java
@@ -1,5 +1,4 @@
 // https://programmers.co.kr/learn/courses/30/lessons/43162
-// 실패
 
 import java.util.HashSet;
 
@@ -8,28 +7,22 @@ class Solution {
         int answer = 0;
         boolean[] network = new boolean[n];
         
-        for (int i=0; i<n; i++) {
-            HashSet<Integer> check = new HashSet<>();
-            if (!network[i]) {
-                for (int j=0; j<i; j++) {
-                    if (computers[i][j] == 1 && network[i] == false) {
-                        network = checkVisit(computers, i, j, network);
-                        answer++;
-                    }
-                }
+        for (int node=0; node<n; node++) {
+            if (!network[node]) {
+                dfs(computers, node, network);
+                answer++;
             }
         }
         
         return answer;
     }
     
-    boolean[] checkVisit(int[][] computers, int i, int j, boolean[] visited) {
-        visited[i] = true;
-        visited[j] = true;
+    boolean[] dfs(int[][] computers, int node, boolean[] visited) {
+        visited[node] = true;
         
-        for (int k=0; k<j; k++) {
-            if (computers[j][k] == 1 && visited[k] == false) {
-                visited = checkVisit(computers, i, j, visited);
+        for (int idx=0; idx<computers.length; idx++) {
+            if (node != idx && computers[node][idx] == 1 && visited[idx] == false) {
+                visited = dfs(computers, idx, visited);
             }
         }
         return visited;
--- programmers/level3/야근 지수/다른방법.java
+++ programmers/level3/야근 지수/다른방법.java
@@ -1,15 +1,9 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12927#
-/*
-    효율설 테스트 1, 2번 실패
- */
- 
-import java.util.Arrays;
 
 class Solution2 {
     public long solution(int n, int[] works) {
         int[] result = new int[works.length];
-        int totalovertime = sumArr(works);
-        totalovertime -= n; 
+        int totalovertime = sumArr(works) - n;
         int time = totalovertime;
         
         if (time <= 0) return 0;
@@ -39,8 +33,8 @@ class Solution2 {
         return sum;
     }
     
-    int sumArr(int[] works) {
-        int sum = 0;
+    long sumArr(int[] works) {
+        long sum = 0;
         for (int work : works) {
             sum += work;
         }
