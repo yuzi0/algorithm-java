d525ee2475f5805733f162d33e41daa0ccae3737
yuzi0 <zag001@naver.com>
2020-03-11 23:53:16 +0900

    2019/12/03 스터디

---
+++ programmers/level2/기능개발.java
@@ -0,0 +1,28 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42586
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.LinkedList;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList<>();
+        Queue<Integer> time = new LinkedList<>();
+        for(int i=0; i<progresses.length; i++) {
+            time.offer((int)Math.ceil((double)(100-progresses[i])/speeds[i]));
+        }
+        System.out.println("time : " + time.toString());
+        
+        while(!time.isEmpty()) {
+            int check = 1;
+            int num = time.poll();
+            while(time.size()!=0 && num>=time.peek()) {
+                check++;
+                time.poll();
+            }
+            answer.add(check);
+        }
+        System.out.println("answer : " + answer.toString());
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
