5d454587d0c9345394d13ba268d4a1cbbc321ff3
yuzi0 <zag001@naver.com>
2020-03-21 21:52:46 +0900

    단체사진 찍기 - 실패 코드 수정

--- programmers/카카오/2017/단체사진 찍기.java
+++ programmers/카카오/2017/단체사진 찍기.java
@@ -1,71 +1,77 @@
 // https://programmers.co.kr/learn/courses/30/lessons/1835
+// 실패
 
-import java.util.LinkedList;
+import java.util.Arrays;
 
 class Solution {
+  public String[] datas;
+  public int leng = 8;
+  public int num = 0;
+    
   public int solution(int n, String[] data) {
-      int answer = 0;
-      LinkedList<Integer> list = new LinkedList<>();
-      boolean[] visit = new boolean[8];
-      
-      // list 1~8까지 채우기
-      //for (int i = 0; i < 8; i++) {
-      //    list.add(i);
-      //}
-      
-      // 순열
-      permutation(list, visit, 8, 8, data, answer); 
-      
-      return answer;
+      char[] list = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+      char[] output = new char[8];
+      boolean[] visited = new boolean[8];
+
+      datas = data;
+
+      permutation(list, output, visited, 0); 
+
+      return num;
   }
     
-    void permutation(LinkedList<Integer> list, boolean[] visit, int n, int r, String[] data, int rt) {
-		if (list.size() == r) { //&& checkData(list, data)) {
-            rt++;
+    void permutation(char[] list, char[] output, boolean[] visited, int depth) {
+		if ( depth == leng && checkData(output) ) {
+            num++;
             return;
         }
         
-		for (int i=0; i<n; i++){
-			if (!visit[i]){//이미 뽑은 것은 뽑지 않도록 체크
-				visit[i] = true;
-				list.add(i);
-				permutation(list, visit, n, r, data, rt);
-				list.removeLast();//해당 넘버를 다시 제거 (즉,뽑지 않고 다음 번호 뽑기위함)
-				visit[i] = false;
+		for (int i = 0; i < leng; i++) {
+			if ( !visited[i] ) {
+				visited[i] = true;
+				output[depth] = list[i];
+				permutation(list, output, visited, depth + 1);
+				visited[i] = false;
 			}
 		}
 	}
     
-    boolean checkData(LinkedList<Integer> list, String[] datas) {
-        for (String data : datas) {
+    boolean checkData(char[] list) {
+        for ( String data : datas ) {
             char chr1 = data.charAt(0);
             char chr2 = data.charAt(3);
-            int n = data.charAt(4) - '0';
-            int compareN = checkBetween(list, chr1, chr2);
+            int compareN = data.charAt(4) - '0';
+            int N = getDistance(list, chr1, chr2);
             
-            switch (data.charAt(3)) {
+            
+            switch ( data.charAt(3) ) {
                 case '=':
-                    if (n != compareN) return false;
+                    if ( compareN == N ) {
+                        System.out.println(Arrays.toString(list));
+                        continue;
+                    }
                 case '<':
-                    if (n >= compareN) return false;
+                    if ( compareN < N ) continue;
                 case '>':
-                    if (n <= compareN) return false;
+                    if ( compareN > N ) continue;
+                default:
+                    return false;
             }
+            //System.out.println("chr1 : " + chr1 + ", chr2 : " + chr2 + ", compareN : " + compareN + ", N : " + N);
         }
         return true;
     }
     
-    int checkBetween(LinkedList<Integer> list, char chr1, char chr2) {
-        char[] arr = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
-        int n = 0;
+    int getDistance(char[] list, char chr1, char chr2) {
+        int n = -1;
         
-        for (int i = 0; i < list.size(); i++) {
-            if (n != 0) n++;
-            
-            if (arr[list.get(i)] == chr1 || arr[list.get(i)] == chr2) {
-                if (n == 0) n++;
-                else break;
-            }
+        for (int i = 0; i < leng; i++) {
+            if ( n != -1 ) {
+                if ( list[i] == chr1 || list[i] == chr2 ) break;
+                n++;
+            } else if ( list[i] == chr1 || list[i] == chr2 ) {
+                n++;
+            } 
         }
         
         return n;
