e049533fa55e3e4e1e18ad087c620be96096386e
yuzi0 <zag001@naver.com>
2020-03-27 23:59:26 +0900

    2020/03/27 스터디 - 브라이언의 고민 추가

---
+++ programmers/level3/브라이언의 고민.java
@@ -0,0 +1,17 @@
+// https://programmers.co.kr/learn/courses/30/lessons/1830
+
+// 입력 형식
+// 입력은 문자열 변수 sentence로 주어진다. 
+// 이 문자열은 영문 대소문자로만 이루어져 있으며, 길이는 1,000 이하이다.
+
+// 출력 형식
+// 입력으로 주어진 광고 문구의 규칙 적용 전 원래 문구를 리턴한다. 
+// 단 원래 문구의 경우 문장 앞뒤의 공백이 없어야 하며, 단어 사이의 공백은 한 글자여야 한다. 
+// 가능한 답이 여러 가지인 경우 그중 하나를 리턴하면 된다. 
+// 규칙에 따른 변환으로 만들 수 없는 문자열이 입력된 경우에는 소문자로 invalid를 리턴한다.
+
+// 예제 입출력
+// sentence         answer
+// HaEaLaLaObWORLDb HELLO WORLD
+// SpIpGpOpNpGJqOqA	SIGONG JOA
+// AxAxAxAoBoBoB	invalid
