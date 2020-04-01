2c5b3c242948f0c14c56e011b48a3a590f7d23aa
yuzi0 <zag001@naver.com>
2020-04-01 23:52:15 +0900

    [2020/04/04] 등굣길 코드 추가

---
+++ programmers/level3/등굣길.java
@@ -0,0 +1,13 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42898
+
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+        // answer : 집에서 학교까지 갈 수 있는 최단 경로의 개수
+        // m, n : 경로의 크기
+        // pubbles : 장애물의 위치
+
+        // DFS인데 오른쪽, 아래로만 움직이게?
+        return answer;
+    }
+}
