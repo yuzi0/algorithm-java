345c7ada335f6b397fde3e810ef960842533fbcf
yuzi0 <zag001@naver.com>
2020-04-09 23:36:55 +0900

    [2020/04/11] '섬 연결하기' 문제 추가

---
+++ programmers/level3/섬 연결하기.java
@@ -0,0 +1,14 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42861?language=java
+// 섬 연결하기
+
+class Solution {
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        // 다리 건설에 필요한 최소 비용 구하기
+
+        // n : 섬의 개수ㄴㄴ
+        // costs : 섬1, 섬2, 섬1과 섬2 사이의 다시 건설 비용
+        // costs 길이는 ((n - 1) * n) / 2 이하이다.
+        return answer;
+    }
+}
