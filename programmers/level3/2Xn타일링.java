cbe0214f4fac891f1a144681597790b56294ce33
yuzi0 <zag001@naver.com>
2020-04-16 23:25:54 +0900

    [2020-04-18] '2Xn타일링' 구상 추가

--- programmers/level3/2Xn타일링.java
+++ programmers/level3/2Xn타일링.java
@@ -3,6 +3,16 @@
 class Solution {
   public int solution(int n) {
       int answer = 0;
+      // 숫자 n을 1과 2로 나누는 방법
+      // 순열과 조합으로 풀어야 하나?
+      
+      // 1 n개, 2 0개인 경우
+      // 1 n-2개, 2 1개인 경우
+      // ...
+      // 1 0개, 2 n/2개인 경우
+      
+      // 위에 대한 모든 경우의 수에 대해서 구하기
+      
       return answer;
   }
 }
