a758b97ca8abaa3a2deb597114ba08e082a33c49
yuzi0 <zag001@naver.com>
2020-04-27 22:41:48 +0900

    [2020/05/02 '추석 트래픽' 구상 추가

---
+++ programmers/level3/추석 트래픽.java
@@ -0,0 +1,13 @@
+// https://programmers.co.kr/learn/courses/30/lessons/17676
+
+class Solution {
+  public int solution(String[] lines) {
+      int answer = 0;
+      // PriorityQueue에 로그의 끝나는 시간을 넣는다.
+      // 새로운 로그가 추가될 때마다,
+      // 새 로그의 시작 시간이랑 queue의 끝나는 시간을 비교해서 시간이 1초 이상 차이나면 queue에서 뺀다.
+      
+      // queue의 최대 크기를 기록해놓고 queue의 사이즈가 0이 되면, 최대 크기를 반환한다.
+      return answer;
+  }
+}
