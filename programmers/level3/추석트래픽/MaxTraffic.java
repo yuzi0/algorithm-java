c8ce232ef65f3d4c96c54c645bf90802e6131bce
yuzi0 <zag001@naver.com>
2020-05-05 21:54:33 +0900

    [2020/05/02] '추석 트래픽' 성공 코드 추가

--- programmers/level3/추석트래픽/MaxTraffic.java
+++ programmers/level3/추석트래픽/MaxTraffic.java
@@ -1,65 +1,60 @@
 // https://programmers.co.kr/learn/courses/30/lessons/17676
 
-// 풀이
-// PriorityQueue에 로그의 끝나는 시간을 넣는다.
-// 새로운 로그가 추가될 때마다, 새 로그의 시작 시간이랑 queue의 끝나는 시간을 비교해서 시간이 1초 이상 차이나면 queue에서 뺀다.
-// queue의 최대 크기를 기록해놓고 queue의 사이즈가 0이 되면, 최대 크기를 반환한다.
-
-// 실패 : 81.8점
-import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Calendar;
-import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.text.ParseException;
-import java.util.Collections;
 
-class Solution {
-	public int solution(String[] lines) {
+class MaxTraffic {
+	public int maxTraffic(String[] lines) {
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
-		PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
-		int max = 0;
+		int leng = lines.length;
+		Long[] startTime = new Long[leng];
+		Long[] endTime = new Long[leng];
+		int max = 1;
 		
-		for (int i = lines.length - 1; i >= 0; i--) {
+		for (int i = 0; i < leng; i++) {
 			String datetime = lines[i].substring(0, 23);
 			int millisec = (int) (Float.valueOf(lines[i].substring(24, lines[i].length() - 1)) * 1000);
-			
-			Calendar cal = Calendar.getInstance();
-			Timestamp original = convertStringToTimestamp(datetime, formatter);
-			cal.setTimeInMillis(original.getTime());
-			Long end = (new Timestamp(cal.getTime().getTime())).getTime();
-			
-			cal.add(Calendar.MILLISECOND, -(millisec - 1));
-			Long start = (new Timestamp(cal.getTime().getTime())).getTime();
-
-			while (true) {
-				if ((pq.isEmpty()) || (pq.peek() - end < 1000)) {
-					break;
+						
+			endTime[i] = toLong(datetime, formatter);
+			startTime[i] = addMiili(endTime[i], 1 - millisec);
+		}
+		
+		for (int i = 0; i < leng; i++) {
+			int tmpMax = 0;
+			for (int j = 0; j < leng; j++) {
+				if (startTime[i] - endTime[j] > 1000) {
+					continue;
 				}
-				pq.poll();
+
+				if(((endTime[i] - startTime[j]) <= 0 && (startTime[j] - endTime[i]) < 1000)
+                    || ((endTime[i] - endTime[j]) <= 0 && (endTime[j] - endTime[i]) < 1000)
+                    || ((endTime[i] - startTime[j]) > 0 && (endTime[i] - endTime[j]) <= 0)) {
+					tmpMax++;
+	            }
 			}
-
-			pq.add(start);
-
-			if (pq.size() > max) {
-				max = pq.size();
+			
+			if (max < tmpMax) {
+				max = tmpMax;
 			}
 		}
 
 		return max;
-
 	}
 
-	public static Timestamp convertStringToTimestamp(String str_date, SimpleDateFormat formatter) {
+	public static Long toLong(String str_date, SimpleDateFormat formatter) {
 		try {
-			Date date = formatter.parse(str_date);
-			java.sql.Timestamp timeStampDate = new Timestamp(date.getTime());
-			return timeStampDate;
-
+			return formatter.parse(str_date).getTime();
 		} catch (ParseException e) {
 			System.out.println("Exception :" + e);
 			return null;
 		}
+	}
+	
+	public static Long addMiili(Long date, int milli) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTimeInMillis(date);
+		cal.add(Calendar.MILLISECOND, milli);
+		return cal.getTime().getTime();
 	}
 }
