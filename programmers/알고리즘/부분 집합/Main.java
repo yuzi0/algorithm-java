b19be0914b79a52af3c195e45f10121d13f9eaa6
yuzi0 <zag001@naver.com>
2020-05-15 23:49:17 +0900

    [2020/05/16] '저울' 성공 코드 추가

---
+++ programmers/알고리즘/부분 집합/Main.java
@@ -0,0 +1,35 @@
+// 출처
+// https://woovictory.github.io/2019/05/24/Algorithm-Set/
+
+public class Main {
+    private static int n = 3;
+    private static boolean[] visited = new boolean[n];
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        getSet(arr, 0);
+    }
+
+    private static void getSet(int[] arr, int index) {
+        if (index == n) {
+            print(arr);
+            return;
+        }
+
+        visited[index] = false;
+        getSet(arr, index + 1);
+
+        visited[index] = true;
+        getSet(arr, index + 1);
+
+    }
+
+    private static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if(visited[i]){
+                System.out.print(arr[i]+" ");
+            }
+        }
+        System.out.println();
+    }
+}
